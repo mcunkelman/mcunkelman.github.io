ca2352d93911eab2a1fe9bffff269744589cb540
mcunkelman <cunkelman.matthew@gmail.com>
2024-01-17 22:15:24 -0500

    Changes to travel files

---
+++ javascript.js
@@ -0,0 +1,70 @@
+// Top level show / hide for Alaska
+function alaskaShowHide() {
+    var x = document.getElementById("alaskaShowHide");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+// Image gallery show / hide for alaska
+  function alaskaGalleryShowHide() {
+    var x = document.getElementById("alaskaTravelGallery");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function icelandShowHide() {
+    var x = document.getElementById("icelandShowHide");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function icelandGalleryShowHide() {
+    var x = document.getElementById("icelandTravelGallery");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function nzShowHide() {
+    var x = document.getElementById("nzShowHide");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function nzGalleryShowHide() {
+    var x = document.getElementById("nzTravelGallery");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function wyShowHide() {
+    var x = document.getElementById("wyShowHide");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function wyGalleryShowHide() {
+    var x = document.getElementById("wyTravelGallery");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
